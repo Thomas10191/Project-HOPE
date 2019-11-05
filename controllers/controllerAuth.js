44b5f2fe421f1163b40f5955023aad9fffdfe63c
HOPE-5 <35104052+erikalira@users.noreply.github.com>
2019-11-05 18:43:23 -0300

    adicionado retorno de id na autenticacao

--- controllers/controllerAuth.js
+++ controllers/controllerAuth.js
@@ -13,7 +13,7 @@ module.exports = {
         if (req.body.email) {
              User.findOne({where : {email: req.body.email, password: req.body.password}}).then(function(value){
                 if (value) {
-                    return res.json({error: false});
+                    return res.json({id: value.dataValues.id});
                 } else {
                     return res.status(401).json({error: true});
                 }
