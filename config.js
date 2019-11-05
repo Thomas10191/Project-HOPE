5759e7776f21d6504a50c7c9df9ee0334be1ea3b
HOPE-5 <35104052+erikalira@users.noreply.github.com>
2019-11-05 19:56:50 -0300

    Corrigida inicialização do docker-compose

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
         port: 3306,
     	database: "hope", //data base of institute
     	username: "postgres", //User rap
-    	password: "Postgres2019!", //Password user rap
+    	password: "passhope", //Password user rap
     	database_type: "postgres"  //database types avaliables:'mysql'|'sqlite'|'postgres'|'mssql'
     },
     API_PORT : 8040
