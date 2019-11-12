6cccc6bf1df642a30186642c25f0885070e47268
HOPE-5 <35104052+erikalira@users.noreply.github.com>
2019-11-12 19:18:48 -0300

    alteracao do modelo user e adicao do sober

---
+++ models/Sober.js
@@ -0,0 +1,25 @@
+
+const Sequelize = require('sequelize');
+const sequelizePaginate = require('sequelize-paginate');
+
+const conection = require(__dirname+'/../connection.js');
+
+var Sober = conection.sequelize.define('tb_sober', {
+    	id: {
+    		type: Sequelize.INTEGER, 
+    		autoIncrement: true, 
+    		primaryKey: true
+    	},
+        user_id: {
+            type: Sequelize.INTEGER
+        }
+        createAt: Sequelize.DATE
+	},{
+        schema: conection.schema,
+        freezeTableName: true,
+        timestamp: true
+	}
+);
+
+sequelizePaginate.paginate(Sober);
+module.exports = Sober;
--- models/User.js
+++ models/User.js
@@ -7,23 +7,53 @@ const conection = require(__dirname+'/../connection.js');
 var User = conection.sequelize.define('tb_user', {
 	id: {
 		type: Sequelize.INTEGER, 
-		autoIncrement: true, 
+		autoIncrement: true,
 		primaryKey: true
 	}, 
 	first_name: { 
-		type: Sequelize.STRING(30)
+		type: Sequelize.STRING(20)
 	}, 
 	last_name: {
-		type: Sequelize.STRING(30)
+		type: Sequelize.STRING(20)
+	},
+	cpf: {
+		type: Sequelize.STRING(11)
+	},
+	age: {
+		type: Sequelize.INT(3)
 	},
 	phone: {
-		type: Sequelize.STRING(20)
+		type: Sequelize.TEXT
 	},
 	email: {
 		type: Sequelize.TEXT
 	},
 	password: {
 		type: Sequelize.TEXT
+	},
+	image_url: {
+		type: Sequelize.TEXT
+	},
+	adress_street: {
+		type: Sequelize.TEXT
+	},
+	adress_city: {
+		type: Sequelize.TEXT
+	},
+	adress_state: {
+		type: Sequelize.TEXT
+	},
+	adress_country: {
+		type: Sequelize.TEXT
+	},
+	adress_zipcode: {
+		type: Sequelize.TEXT
+	},
+	data: {
+		type: Sequelize.JSON
+	},
+	type_user_id: {
+		type: Sequelize.INTEGER
 	}
 },{
     schema: conection.schema,
